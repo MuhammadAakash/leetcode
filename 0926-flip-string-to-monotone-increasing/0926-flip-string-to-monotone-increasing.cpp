3afc4ea50df0b3867dae9a87b9bc94700f715d2d
Mohammad Aakash <64223049+MuhammadAakash@users.noreply.github.com>
2023-01-17 21:44:23 +0500

    Time: 20 ms (93.67%), Space: 11.1 MB (82.03%) - LeetHub

--- 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.cpp
+++ 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.cpp
@@ -1,19 +1,32 @@
 class Solution {
 public:
     int minFlipsMonoIncr(string s) {
-        int m = 0;
+        // int m = 0;
+        // for(char c: s) {
+        //     if(c == '0') {
+        //         ++m;
+        //     }
+        // }
+        // int ans = m;
+        // for(char c: s) {
+        //     if(c == '0') {
+        //         ans = min(ans, --m);
+        //     }
+        //     else {
+        //         ++m;
+        //     }
+        // }
+        // return ans;
+        
+        // 2nd Approach
+        
+        int ans = 0, num = 0;
         for(char c: s) {
             if(c == '0') {
-                ++m;
-            }
-        }
-        int ans = m;
-        for(char c: s) {
-            if(c == '0') {
-                ans = min(ans, --m);
+                ans = min(num, ans + 1);
             }
             else {
-                ++m;
+                ++num;
             }
         }
         return ans;
