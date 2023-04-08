0bad4c5284c2b9da8c67f19ec3c4de26bc55a380
Mohammad Aakash <64223049+MuhammadAakash@users.noreply.github.com>
2023-04-08 23:33:41 +0500

    Time: 7 ms (61.34%), Space: 9 MB (34.83%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    unordered_map<Node* , Node*> mp; // declaring map, to check whwther we have a copy of node or not and also to store copy
+    
+    Node* cloneGraph(Node* node) {
+        if(node == NULL) // if node is null, then simply return null
+        {
+            return NULL;
+        }
+        
+        // for a node, we will check whether we already creates a copy of thiis or not. If it is present in map that means we already creates a copy of this.
+        //But if not present in map, that means we have not a copy of this.
+        // Also, if we create a copy, then being a good neighbor, we find whether our neighbor have a copy or not, so we will travel all around our adjcant.
+        
+        if(mp.find(node) == mp.end()) // if not present in map
+        {
+            mp[node] = new Node(node -> val, {}); // make a copy
+            
+            for(auto adj: node -> neighbors) // travel in adjcant
+            {
+                mp[node] -> neighbors.push_back(cloneGraph(adj)); //add copy
+            }
+        }
+        
+        return mp[node]; // and at last, return mp[node] as till now we clone our whole graph
+        
+    }
+};
