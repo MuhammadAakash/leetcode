ad70869768374623a5fc42e2161db71618ba5c57
Mohammad Aakash <64223049+MuhammadAakash@users.noreply.github.com>
2023-05-31 00:59:47 +0500

    Time: 86 ms (87.84%), Space: 43.5 MB (49.26%) - LeetHub

---
+++ 0705-design-hashset/0705-design-hashset.cpp
@@ -0,0 +1,21 @@
+class MyHashSet {
+private:
+    std::vector<bool> mp;
+
+public:
+    MyHashSet() {
+        mp.resize(1000001, false);
+    }
+
+    void add(int key) {
+        mp[key] = true;
+    }
+
+    void remove(int key) {
+        mp[key] = false;
+    }
+
+    bool contains(int key) {
+        return mp[key];
+    }
+};
