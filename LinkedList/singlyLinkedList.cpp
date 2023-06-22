9a2e16cbfc1c239ac2113cef086a4ad57fbb07ba
Mohammad Aakash <muhammadaakash249@gmail.com>
2023-06-22 22:40:19 +0500

    insertion at end of teh linked list

--- LinkedList/singlyLinkedList.cpp
+++ LinkedList/singlyLinkedList.cpp
@@ -20,6 +20,12 @@ void insertAtHead(Node *&head, int d)
     temp->next = head;
     head = temp;
 };
+void insertAtTail(Node *&tail, int d)
+{
+    Node *temp = new Node(d);
+    tail->next = temp;
+    tail = temp;
+}
 
 void print(Node *&head)
 {
@@ -37,13 +43,14 @@ int main()
 {
     Node *node1 = new Node(19);
     Node *head = node1;
+    Node *tail = node1;
     print(head);
 
-    insertAtHead(head, 20);
+    insertAtTail(tail, 20);
 
     print(head);
 
-    insertAtHead(head, 30);
+    insertAtTail(tail, 30);
 
     print(head);
 }
