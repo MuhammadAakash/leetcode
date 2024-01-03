1c80bb497e7734558c101bd25349337fbd9e30d1
Mohammad Aakash <64223049+MuhammadAakash@users.noreply.github.com>
2024-01-04 01:07:39 +0500

    Time: 91 ms (56.14%), Space: 29.2 MB (24.95%) - LeetHub

---
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numberOfBeams(vector<string>& bank) {
+        int prev, ans = 0;
+        for(string s: bank) {
+            int count = 0;
+            for(char c: s) {
+                if(c == '1') {
+                    count++;
+                }
+            }
+            if(count != 0) {
+                ans += (prev * count);
+                prev = count;
+            }
+        }
+        return ans;
+    }
+};
