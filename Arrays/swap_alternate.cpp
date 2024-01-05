81179723e08a1bcc0559ba74667e8c9ec4f8d996
Muhammad Aakash <muhammadaakash249@gmail.com>
2024-01-05 08:15:06 +0500

    Written swap alternatives functiona again

--- Arrays/swap_alternate.cpp
+++ Arrays/swap_alternate.cpp
@@ -1,38 +1,38 @@
-#include<iostream>
-#include<stdio.h>
+#include <stdio.h>
+#include <iostream>
 
 using namespace std;
 
-void printAll(int arr[], int size){
-    for(int i = 0; i < size; i++){
-        cout<< arr[i] << ' ';
-
+void printArray(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
     }
-    cout<< endl;
+    cout << endl;
 }
 
-
-void swap_Allternate(int arr[], int size){
-    for(int i = 0; i < size; i+=2){
-        if(i+1 < size){
-            // swap(arr[i], arr[i+1]);
+void swapAlternatives(int arr[], int size)
+{
+    for (int i = 0; i < size; i += 2)
+    {
+        if (i + 1 < size)
+        {
             int temp = arr[i];
             arr[i] = arr[i + 1];
             arr[i + 1] = temp;
         }
     }
 }
-void swap_number(){
-    
-}
 
-int main(){
-    int evenArray[8] = {3,7,2,9,5,0,1,66};
-    int oddArray[5] = {55,33,11,66,00};
+int main()
+{
+    int evenArray[8] = {2, 1, 4, 5, 53, 25, 55, 33};
+    int oddArray[9] = {43, 1, 53, 22, 66, 4, 8, 32, 78};
 
-    swap_Allternate(evenArray, 8);
-    printAll(evenArray, 8);
+    swapAlternatives(evenArray, 8);
+    printArray(evenArray, 8);
 
-    swap_Allternate(oddArray, 5);
-    printAll(oddArray, 5);
+    swapAlternatives(oddArray, 9);
+    printArray(oddArray, 9);
 }
